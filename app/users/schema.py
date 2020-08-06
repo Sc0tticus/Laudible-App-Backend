42953ff511b76da6f3f51f121c2fdb287236fe04
github username <github email address>
2020-08-06 11:34:52 -0600

    All code thru Ch. 18 working. Auth works with GraphQL

--- app/users/schema.py
+++ app/users/schema.py
@@ -6,6 +6,13 @@ from graphene_django import DjangoObjectType
 class UserType(DjangoObjectType):
   class Meta:
     model = get_user_model()
+    # only_fields = ('id', 'email', 'password', 'username')
+
+class Query(graphene.ObjectType):
+  user = graphene.Field(UserType, id=graphene.Int(required=True))
+
+  def resolve_user(self, info, id):
+    return get_user_model().objects.get(id=id)
 
 class CreateUser(graphene.Mutation):
   user = graphene.Field(UserType)
@@ -16,12 +23,13 @@ class CreateUser(graphene.Mutation):
     email = graphene.String(required=True)
 
   def mutate(self, info, username, password, email):
-    get_user_model()(
-      username=username,
-      email=email
+    user = get_user_model()(
+        username=username,
+        email=email
     )
-  user.set_password(password)
-  user.save()
+    user.set_password(password)
+    user.save()
+    return CreateUser(user=user)
 
 class Mutation(graphene.ObjectType):
   create_user = CreateUser.Field()
